a75841e4aa4872202363d47e5d756563cfd10396
muhsinmikoya-pixel <muhsinmikoya@gmail.com>
2026-08-22 17:51:45 +0300

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,43 @@
+import { initializeApp } from
+"https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword
+} from
+"https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
+import {
+  getFirestore
+} from
+"https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCfxi6jdRj1EAhUtOjg-ChRSuZTXPkWIDI",
+  authDomain: "avyra-666da.firebaseapp.com",
+  projectId: "avyra-666da",
+  storageBucket: "avyra-666da.firebasestorage.app",
+  messagingSenderId: "238523673454",
+  appId: "1:238523673454:web:afc7dea15dff6afc756f7f",
+  measurementId: "G-XZ996JZR6N"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+const googleProvider = new GoogleAuthProvider();
+
+export {
+  app,
+  auth,
+  db,
+  googleProvider,
+  signInWithPopup,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword
+};
